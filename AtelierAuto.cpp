eba3b35e826884cad51c88d14898cde59706ce55
dianac22 <diana.caraguis@stud.acs.upb.ro>
2023-07-04 21:59:41 +0300

    Class AtelierAuto updated

--- AtelierAuto.cpp
+++ AtelierAuto.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <ctime>
+#include <sstream>
+#include <chrono>
 #include "Angajat.cpp"
 using namespace std;
 
@@ -19,6 +22,47 @@ public:
     }
 
     void adaugareAngajat(string nume, string prenume, string dataNasterii, string dataAngajarii, float coeficient) {
+        // Verificam varsta angajatului - sa aiba cel putin 18 ani
+        tm tmNasterii = {};
+        istringstream issNasterii(dataNasterii);
+        string day, month, year;
+        getline(issNasterii, day, '.');
+        getline(issNasterii, month, '.');
+        getline(issNasterii, year);
+        tmNasterii.tm_mday = std::stoi(day);
+        tmNasterii.tm_mon = std::stoi(month) - 1;
+        tmNasterii.tm_year = std::stoi(year) - 1900;
+
+        tm tmAngajarii = {};
+        istringstream issAngajarii(dataAngajarii);
+        getline(issAngajarii, day, '.');
+        getline(issAngajarii, month, '.');
+        getline(issAngajarii, year);
+        tmAngajarii.tm_mday = std::stoi(day);
+        tmAngajarii.tm_mon = std::stoi(month) - 1;
+        tmAngajarii.tm_year = std::stoi(year) - 1900;
+
+        time_t tNasterii = mktime(&tmNasterii);
+        time_t tAngajarii = mktime(&tmAngajarii);
+
+        double ageSeconds = difftime(tAngajarii, tNasterii);
+        double ageYears = ageSeconds / (60 * 60 * 24 * 365.25); // an bisect
+
+        if (ageYears < 18) {
+            cout << "Angajatul trebuie sa aiba minim 18 ani la data angajarii.\n";
+            return;
+        }
+
+        // Verificam data angajarii pentru a nu putea fi din viitor
+        time_t currentTime = chrono::system_clock::to_time_t(chrono::system_clock::now());
+        tm* tmCurrent = localtime(&currentTime);
+
+        if (tmAngajarii.tm_year > tmCurrent->tm_year || (tmAngajarii.tm_year == tmCurrent->tm_year && tmAngajarii.tm_mon > tmCurrent->tm_mon) ||
+            (tmAngajarii.tm_year == tmCurrent->tm_year && tmAngajarii.tm_mon == tmCurrent->tm_mon && tmAngajarii.tm_mday > tmCurrent->tm_mday)) {
+            cout << "Data de angajare nu poate fi în viitor.\n";
+            return;
+        }
+
         angajati.push_back(Angajat(idCounter++, nume, prenume, dataNasterii, dataAngajarii, coeficient));
         cout << "Angajatul a fost adaugat cu succes.\n";
     }
